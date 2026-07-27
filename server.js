46b5e1c1d9b9ef0ecada73fa9b8c8cb079d98a69
abineshthangasamy <abineshrenga@gmail.com>
2026-07-27 10:04:21 +0530

    Inital Commit

---
+++ server.js
@@ -0,0 +1,46 @@
+import cors from 'cors';
+import next from 'next';
+import Pusher from 'pusher';
+import express from 'express';
+import bodyParser from 'body-parser';
+import 'dotenv/config';
+import Sentiment from 'sentiment';
+    
+    const dev = process.env.NODE_ENV !== 'production';
+    const port = process.env.PORT || 3000;
+    
+    const app = next({ dev });
+    const handler = app.getRequestHandler();
+    const sentiment = new Sentiment();
+    
+    const pusher = new Pusher({
+      appId: process.env.PUSHER_APP_ID,
+      key: process.env.PUSHER_APP_KEY,
+      secret: process.env.PUSHER_APP_SECRET,
+      cluster: process.env.PUSHER_APP_CLUSTER,
+      useTLS: true
+    });
+    
+    app.prepare()
+      .then(() => {
+      
+        const server = express();
+        
+        server.use(cors());
+        server.use(bodyParser.json());
+        server.use(bodyParser.urlencoded({ extended: true }));
+        
+        server.all('/{*splat}', (req, res) => {
+          return handler(req, res);
+        });
+        
+        server.listen(port, err => {
+          if (err) throw err;
+          console.log(`> Ready on http://localhost:${port}`);
+        });
+        
+      })
+      .catch(ex => {
+        console.error(ex.stack);
+        process.exit(1);
+      });
